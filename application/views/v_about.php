0491f3c2d924648f29488469952191bffbebade4
shakafqrm <shakafiqri1.gmail.com>
2022-10-06 16:08:16 +0700

    first commit

---
+++ application/views/v_about.php
@@ -0,0 +1,28 @@
+<section>
+<li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
+ <li><a href="<?php echo 
+base_url().'index.php/web/about' ?>">About</a></li>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Albertt</li>
+        <li>Nama Belakang : James</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Spain</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Voli</li>
+    </ul>
+</section>
